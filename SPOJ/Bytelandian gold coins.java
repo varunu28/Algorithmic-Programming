a3ea941f03d2f2ab51b4556008d9d1e284671ff0
Varun Upadhyay <varun.u28@gmail.com>
2015-12-11 06:28:46 +0530

    Fitst DP Problem

---
+++ SPOJ/Bytelandian gold coins.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    private static Map<Integer, Long> table = new HashMap<Integer, Long>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            out.println(maxDollars(n));
+        }
+        out.flush();
+    }
+
+    public static long maxDollars(int n) {
+        if (n < 12)
+            return n;
+        if (table.containsKey(n))
+            return table.get(n);
+
+        long maxDollarsOfNCoins = maxDollars(n/2)+maxDollars(n/3)+maxDollars(n/4);
+
+        table.put(n, maxDollarsOfNCoins);
+
+        return maxDollarsOfNCoins;
+    }
+}
