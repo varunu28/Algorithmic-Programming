e3820fd4e9578aff3d049258d9fcad4e41fde0d3
Varun Upadhyay <varun.u28@gmail.com>
2015-12-29 21:07:39 +0530

    CodeForces Practise 12/29/2015

---
+++ Codeforces/Practise/41A - Translation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ctype.h>
+#define in cin>>
+#define out cout<<
+#define M 100002
+#define FOR(l)   for(int k=0; k<l; k++)
+#define Si set<int>
+#define Mii map<int,int>
+#define Mss map<string,string>
+#define Vi vector<int>
+#define Di deque<int>
+#define max(a,b) ( (a) > (b) ? (a) : (b))
+#define miN(a,b) ( (a) < (b) ? (a) : (b))
+
+using namespace std;
+
+int main()
+{
+    string t;
+    string s;
+    in s;
+    in t;
+    int i,j;
+    
+    char c;
+    for (i = 0, j = s.length() - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+    
+    if(s==t)
+    {
+        out "YES";
+    }
+    else
+    {
+        out "NO";
+    }
+    return 0;
+}
