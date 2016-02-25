c3df8b087b8ee10c8835eb938a0d46ee4b9a5d14
Varun Upadhyay <varun.u28@gmail.com>
2016-02-25 07:01:13 +0530

    Finishing Day 5

---
+++ Hackerrank/7 Days of Javascript/Day 5 For . .js
@@ -0,0 +1,9 @@
+//Write your code below this line.
+'use strict';
+for(let i of my_array)
+    {
+        if(i===i.split("").reverse().join(""))
+        {
+            console.log(i);
+        }
+    }
