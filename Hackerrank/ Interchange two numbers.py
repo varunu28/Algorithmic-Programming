e6cdfebf0794a3891a6699227e4e8596fb0c00e8
Varun Upadhyay <varun.u28@gmail.com>
2016-05-14 21:41:28 +0530

    Final merge

--- Hackerrank/ Interchange two numbers.py
+++
@@ -1,6 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-a=int(raw_input())
-b=int(raw_input())
-t=(a,b)
-print t[1]
-print t[0]
--- Hackerrank/ Python: Division.py
+++
@@ -1,5 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-a=int(raw_input())
-b=int(raw_input())
-print a/b
-print float(a)/float(b)
