956c126c9ad8cbbbf31d5bbc4cff0cd1e2faf39c
Varun Upadhyay <varun.u28@gmail.com>
2016-06-11 09:44:31 +0530

    Started with Python Strings

---
+++ Hackerrank/Python/String/sWAP cASE.py
@@ -0,0 +1,14 @@
+inp_str = input()
+out_str = ''
+i = 0
+
+while i < len(inp_str):
+    if inp_str[i].islower():
+        out_str += inp_str[i].upper()
+    elif inp_str[i].isupper():
+        out_str += inp_str[i].lower()
+    else:
+        out_str += inp_str[i]
+    i+=1
+
+print(out_str)
