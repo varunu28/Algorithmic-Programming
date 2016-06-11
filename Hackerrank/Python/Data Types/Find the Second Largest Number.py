8485b5152212f916f912fc6683209cc6f33b4da9
Varun Upadhyay <varun.u28@gmail.com>
2016-06-11 08:03:48 +0530

    Finished Python Data Types

--- Hackerrank/Python/Data Types/Find the Second Largest Number.py
+++ Hackerrank/Python/Data Types/Find the Second Largest Number.py
@@ -2,4 +2,23 @@ m = int(input())
 a = input()
 lis = a.split()
 m_list = list(map(int, lis))
-print(sorted(m_list)[m-2])
+a = []
+j=0
+
+while j < len(m_list):
+    if m_list[j]!=m_list[j+1]:
+        largest = max(m_list[j], m_list[j+1])
+        largest2 = min(m_list[j], m_list[j+1])
+        break
+    j+=1
+
+for i in m_list:
+    if i > largest:
+        largest2 = largest
+        largest = i
+    elif i<largest and i>largest2:
+        largest2 = i
+
+print(largest2)
+        
+    
---
+++ Hackerrank/Python/Data Types/Nested Lists.py
@@ -0,0 +1,36 @@
+n = int(input())
+arr = []
+
+while n>0:
+    temp = []
+    name = input()
+    grade = float(input())
+    temp.append(name)
+    temp.append(grade)
+    arr.append(temp)
+    n-=1
+
+grade_arr=[]
+
+for i in arr:
+    grade_arr.append(i[1])
+
+grade_arr.sort()
+
+m = 0
+while m < len(grade_arr):
+    if grade_arr[m] != grade_arr[m+1]:
+        second_low = grade_arr[m+1]
+        break
+    m+=1
+
+names = []
+
+for j in arr:
+    if j[1] == second_low:
+        names.append(j[0])
+
+names.sort()
+
+for k in names:
+    print(k)
