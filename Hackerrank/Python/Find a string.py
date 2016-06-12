84f4fc7beee2f9b78a1e1c8028113be0e75211a7
Varun Upadhyay <varun.u28@gmail.com>
2016-06-12 11:49:26 +0530

    Create Find a string.py

---
+++ Hackerrank/Python/Find a string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+main_str = input()
+sub_str = input()
+i = 0
+count = 0
+
+while i < len(main_str)-len(sub_str) + 1:
+	if main_str[i:i+len(sub_str)] == sub_str:
+		count+=1
+	i+=1
+print(count)
