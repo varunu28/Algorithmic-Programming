8481bd1cf20363d49097deec1db1589307c32168
Varun Upadhyay <varun.u28@gmail.com>
2016-06-21 09:02:42 +0530

    Working on Python Sets

---
+++ Hackerrank/Python/Sets/Set_Discard_Remove_Pop.py
@@ -0,0 +1,21 @@
+n = int(input())
+s = set(map(int, input().split())) 
+N = int(input())
+i = 0
+while i < N:
+    l = input().split()
+    if len(l) == 1:
+        if l[0] == 'pop':
+            s.pop()
+    else:
+        if l[0] == 'remove':
+            s.remove(int(l[1]))
+        else:
+            s.discard(int(l[1]))
+    i+=1
+sum = 0
+for j in s:
+    sum += j
+
+print(sum)
+  
