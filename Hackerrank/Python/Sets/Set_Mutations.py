14a7e4106d12f5c88d1cd37b8ac0d8562873b6c1
Varun Upadhyay <varun.u28@gmail.com>
2016-06-22 06:41:19 +0530

    Create Set_Mutations.py

---
+++ Hackerrank/Python/Sets/Set_Mutations.py
@@ -0,0 +1,19 @@
+n = int(input())
+A = set(map(int, input().split())) 
+
+N = int(input())
+
+for i in range(N):
+    s = input().split()
+    temp = set(map(int, input().split())) 
+    if s[0] == 'intersection_update':
+        A.intersection_update(temp)
+    elif s[0] == 'update':
+        A.update(temp)
+    elif s[0] == 'symmetric_difference_update':
+        A.symmetric_difference_update(temp)
+    elif s[0] == 'difference_update':
+        A.difference_update(temp)
+
+print(sum(A))
+    
