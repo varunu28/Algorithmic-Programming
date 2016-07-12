bacd1a9955afc8305da6b105c46afbaa52829aa4
Varun Upadhyay <varun.u28@gmail.com>
2016-07-12 21:54:59 +0530

    Started with itertools

---
+++ Hackerrank/Python/Itertools/itertools_product.py
@@ -0,0 +1,18 @@
+from itertools import product
+l = map(int, input().strip().split(' '))
+A = []
+for p in l:
+	A.append(p)
+
+l = map(int, input().strip().split(' '))
+B = []
+for p in l:
+	B.append(p)
+
+temp = list(product(A, B))
+ans = ''
+
+for x in temp:
+	ans = ans + str(x) + ' '
+
+print(ans)
