80bae80a7b3cfe6d08b63a3d7e297c1ac8b49062
varunu28 <varun.u28@gmail.com>
2016-08-08 22:34:04 +0530

    Finished itertools

---
+++ Hackerrank/Python/Itertools/Iterables and Iterators.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+
+n = int(input())
+inp = list(map(str, input().strip().split(' ')))
+k = int(input())
+full_list = []
+count = 0
+total = 0
+
+for i in list(combinations(inp, k)):
+	full_list.append(''.join(i))
+	if 'a' in i:
+		count += 1
+	total += 1
+	
+
+ans = float(count)/float(total)
+print("{0:.3f}".format(ans))
