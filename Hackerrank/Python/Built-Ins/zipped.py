56bd7a9028de3d0fb1096aa354b7a0a374bd01cd
varunu28 <varun.u28@gmail.com>
2016-08-11 00:16:35 +0530

    Started with Built-ins on Hackerrank

---
+++ Hackerrank/Python/Built-Ins/zipped.py
@@ -0,0 +1,9 @@
+N, X = [int(x) for x in input().split()]
+
+scores = []
+
+for i in range(X):
+	l = list(map(float, input().strip().split(' ')))
+	scores.append(l)
+
+print(*[sum(student)/len(student) for student in zip(*scores)],sep='\n')
