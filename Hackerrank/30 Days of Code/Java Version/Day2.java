83c9405bd318b788ffd66d9b1c54d96253c10378
varunu28 <varun.u28@gmail.com>
2016-09-07 08:53:28 +0530

    Finished Day 2 of Java

---
+++ Hackerrank/30 Days of Code/Java Version/Day2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        double res = mealCost + (mealCost*tipPercent)/100 + (mealCost*taxPercent)/100;
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(res);
+      
+        // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
+}
