9efd86d49c516213dd845f6b0d34d2702880560e
Varun Upadhyay <varun.u28@gmail.com>
2016-12-02 08:14:47 +0530

    Update Dec Easy_Digit Problem.py

--- Hackerearth/Dec Easy_Digit Problem.py
+++ Hackerearth/Dec Easy_Digit Problem.py
@@ -1,3 +1,23 @@
+# Digit Problem
+'''
+Problem Description: This time your task is simple.
+
+Given two integers X and K, find the largest number that can be formed by changing digits at atmost K places in the number X.
+
+Input:
+
+First line of the input contains two integers X and K separated by a single space.
+
+Output:
+
+Print the largest number formed in a single line.
+
+Constraints:
+
+    1≤X≤1018
+    0≤K≤9
+'''
+
 a,b = (int(i) for i in input().split())
 a_str = list(str(a))
 i = 0
