c19e1e9b4306a126e9cdcfa5442e631ff0ddfb25
varunu28 <varun.u28@gmail.com>
2017-06-22 07:40:02 +0530

    Solved 1 from String and 3 from classes of C++ section on HR

---
+++ Hackerrank/C++/Strings/StringStream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+   // Complete this function
+    stringstream ss(str);
+    vector<int> ivec;
+    int t;
+    char ch;
+    while (ss >> t) {
+        ivec.push_back(t);
+        ss >> ch;
+    }
+    return ivec;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
