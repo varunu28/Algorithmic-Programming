5add945f6dce3c53cf22d900a11dc0a8676452d5
Varun Upadhyay <varun.u28@gmail.com>
2018-08-05 15:28:01 -0700

    Started with InterviewKit at HR

---
+++ Hackerrank/Interview Preparation Kit/Arrays/Array:Left Rotation.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        d = d % a.length;
+        int[] copy = new int[d];
+        int[] ans = new int[a.length];
+        int k = 0;
+        
+        for (int i=0; i<d; i++) {
+            copy[i] = a[i];
+        }
+        
+        for (int i=d; i<a.length; i++) {
+            ans[k++] = a[i];
+        }
+        
+        for (int i=0; i<copy.length; i++) {
+            ans[k++] = copy[i];
+        } 
+
+        return ans;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
